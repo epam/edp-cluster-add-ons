bf10ecbd4a186455149a16195a1c65f227e8ad93
Sergiy Kulanov <sergiy_kulanov@epam.com>
2023-12-12 12:34:41 +0200

    chore: Add-ons table now contains versions (#32)
    Signed-off-by: Sergiy Kulanov <sergiy_kulanov@epam.com>
    Change-Id: Idaaed2e3c9162d58cb2c8c2f825fa54a69ee89a8

--- hack/update_readme.py
+++ hack/update_readme.py
@@ -1,12 +1,30 @@
 import yaml
 from tabulate import tabulate
+import os
 
-# Load the YAML file
+# Load the main YAML file
 with open('./chart/values.yaml', 'r') as file:
     data = yaml.safe_load(file)
 
-# Extract the data
-table_data = [{'Component': key, 'createNamespace': value.get('createNamespace', 'N/A'), 'enable': value.get('enable', 'N/A')} for key, value in data.items()]
+# Initialize the list for the table data
+table_data = []
+
+# Iterate over the data
+for key, value in data.items():
+    # Initialize the row data with placeholders
+    row = {'Component': key, 'version': 'N/A', 'appVersion': 'N/A', 'createNamespace': value.get('createNamespace', 'N/A'), 'enable': value.get('enable', 'N/A')}
+
+    # Try to open the Chart.yaml file for the component
+    try:
+        with open(f'./add-ons/{key}/Chart.yaml', 'r') as file:
+            chart_data = yaml.safe_load(file)
+            row['version'] = chart_data.get('version', 'N/A')
+            row['appVersion'] = chart_data.get('appVersion', 'N/A')
+    except FileNotFoundError:
+        pass
+
+    # Add the row to the table data
+    table_data.append(row)
 
 # Generate the markdown table
 table = tabulate(table_data, headers='keys', tablefmt='pipe')
