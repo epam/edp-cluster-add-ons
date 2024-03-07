e3f190e370511d26bac2176f570ca46edfb2e9a2
Sergiy Kulanov <sergiy_kulanov@epam.com>
2024-03-07 12:00:35 +0200

    docs: Update versions and changelog (#55)
    Align script to the latest changes
    
    Signed-off-by: Sergiy Kulanov <sergiy_kulanov@epam.com>
    Change-Id: I1d0cf089b2848cb22f1ad5f434e5455626ce72eb

--- hack/update_readme.py
+++ hack/update_readme.py
@@ -11,20 +11,22 @@ table_data = []
 
 # Iterate over the data
 for key, value in data.items():
-    # Initialize the row data with placeholders
-    row = {'Component': key, 'version': 'N/A', 'appVersion': 'N/A', 'createNamespace': value.get('createNamespace', 'N/A'), 'enable': value.get('enable', 'N/A')}
+    # Check if value is a dictionary
+    if isinstance(value, dict):
+        # Initialize the row data with placeholders
+        row = {'Component': key, 'version': 'N/A', 'appVersion': 'N/A', 'createNamespace': value.get('createNamespace', 'N/A'), 'enable': value.get('enable', 'N/A')}
 
-    # Try to open the Chart.yaml file for the component
-    try:
-        with open(f'./add-ons/{key}/Chart.yaml', 'r') as file:
-            chart_data = yaml.safe_load(file)
-            row['version'] = chart_data.get('version', 'N/A')
-            row['appVersion'] = chart_data.get('appVersion', 'N/A')
-    except FileNotFoundError:
-        pass
+        # Try to open the Chart.yaml file for the component
+        try:
+            with open(f'./add-ons/{key}/Chart.yaml', 'r') as file:
+                chart_data = yaml.safe_load(file)
+                row['version'] = chart_data.get('version', 'N/A')
+                row['appVersion'] = chart_data.get('appVersion', 'N/A')
+        except FileNotFoundError:
+            pass
 
-    # Add the row to the table data
-    table_data.append(row)
+        # Add the row to the table data
+        table_data.append(row)
 
 # Generate the markdown table
 table = tabulate(table_data, headers='keys', tablefmt='pipe')
